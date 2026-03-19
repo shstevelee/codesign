50509716fcdb19860346138131a8e73e2ecc1816
Seunghyeok Lee <seunghye@bluey.lan.local.cmu.edu>
2026-03-19 19:11:28 -0400

    debug: debugging with skip flags

--- test/automated_test_scripts/automated-scripts.sh
+++ test/automated_test_scripts/automated-scripts.sh
@@ -10,7 +10,7 @@ set -eo pipefail
 
 shopt -s expand_aliases
 
-OPENROAD_PRE_INSTALLED=1 SCALEHLS_PRE_INSTALLED=1 STREAMHLS_PRE_INSTALLED=1 source full_env_start.sh
+OPENROAD_PRE_INSTALLED=1 SCALEHLS_PRE_INSTALLED=1 STREAMHLS_PRE_INSTALLED=1 source full_env_start.sh --skip-openroad
 
 # Run regression and propagate its exit code directly.
 # Use 'set +e' so the script can capture the exit code instead of exiting immediately.
